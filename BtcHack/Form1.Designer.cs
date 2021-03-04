eb0282383414e7c6ff58241ca0f06dd289b943f2
Bluzume <39113159+KuromeSan@users.noreply.github.com>
2021-03-05 01:12:45 +1300

    Add files via upload

---
+++ BtcHack/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿
+namespace BtcHack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.walleeet = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.privatKey = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.amnt = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progresm = new System.Windows.Forms.ProgressBar();
+            this.hack = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.progress = new System.Windows.Forms.Label();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::BtcHack.Properties.Resources.Bitcoin_logo_white_background;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 113);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Lavender;
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(12, 146);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "BIT COIN HACK";
+            // 
+            // walleeet
+            // 
+            this.walleeet.BackColor = System.Drawing.Color.Olive;
+            this.walleeet.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.walleeet.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.walleeet.Location = new System.Drawing.Point(12, 223);
+            this.walleeet.Name = "walleeet";
+            this.walleeet.Size = new System.Drawing.Size(773, 20);
+            this.walleeet.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Lime;
+            this.label2.ForeColor = System.Drawing.Color.Maroon;
+            this.label2.Location = new System.Drawing.Point(12, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "WALLET ADDRESS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.DodgerBlue;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label3.ForeColor = System.Drawing.Color.Yellow;
+            this.label3.Location = new System.Drawing.Point(41, 280);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "PRIVATE KEY";
+            // 
+            // privatKey
+            // 
+            this.privatKey.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.privatKey.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.privatKey.Location = new System.Drawing.Point(13, 296);
+            this.privatKey.Name = "privatKey";
+            this.privatKey.Size = new System.Drawing.Size(773, 13);
+            this.privatKey.TabIndex = 5;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.SteelBlue;
+            this.checkBox1.ForeColor = System.Drawing.Color.Crimson;
+            this.checkBox1.Location = new System.Drawing.Point(106, 346);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(92, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "use proxy vpn";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.BackColor = System.Drawing.Color.Thistle;
+            this.checkBox2.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.checkBox2.Location = new System.Drawing.Point(310, 383);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(128, 25);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "rewrote the firewalls";
+            this.checkBox2.UseVisualStyleBackColor = false;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.BackColor = System.Drawing.Color.Yellow;
+            this.checkBox3.ForeColor = System.Drawing.Color.Blue;
+            this.checkBox3.Location = new System.Drawing.Point(577, 357);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(128, 25);
+            this.checkBox3.TabIndex = 8;
+            this.checkBox3.Text = "use encrypton";
+            this.checkBox3.UseVisualStyleBackColor = false;
+            // 
+            // amnt
+            // 
+            this.amnt.BackColor = System.Drawing.Color.GreenYellow;
+            this.amnt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.amnt.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.amnt.FormattingEnabled = true;
+            this.amnt.Items.AddRange(new object[] {
+            "-5989892btc",
+            "-1000btc",
+            "-2000000btc",
+            "0btc",
+            "1929819581btc",
+            "1000000000000000000000000000btc",
+            "9897878989btc",
+            "NaNbtc",
+            "91859185918btc",
+            "99999999999999999999999999999999999999999btc",
+            "1btc",
+            "928195198591btc",
+            "+Infinitybtc"});
+            this.amnt.Location = new System.Drawing.Point(238, 462);
+            this.amnt.Name = "amnt";
+            this.amnt.Size = new System.Drawing.Size(287, 21);
+            this.amnt.TabIndex = 9;
+            this.amnt.Text = "-5989892btc";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.label4.Location = new System.Drawing.Point(158, 446);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "How much btcs";
+            // 
+            // progresm
+            // 
+            this.progresm.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.progresm.Location = new System.Drawing.Point(28, 516);
+            this.progresm.Name = "progresm";
+            this.progresm.Size = new System.Drawing.Size(694, 23);
+            this.progresm.TabIndex = 11;
+            // 
+            // hack
+            // 
+            this.hack.BackColor = System.Drawing.Color.Blue;
+            this.hack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.hack.ForeColor = System.Drawing.Color.Yellow;
+            this.hack.Location = new System.Drawing.Point(458, 545);
+            this.hack.Name = "hack";
+            this.hack.Size = new System.Drawing.Size(247, 23);
+            this.hack.TabIndex = 12;
+            this.hack.Text = "Hack btcs";
+            this.hack.UseVisualStyleBackColor = false;
+            this.hack.Click += new System.EventHandler(this.hack_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Purple;
+            this.label5.ForeColor = System.Drawing.Color.Salmon;
+            this.label5.Location = new System.Drawing.Point(12, 325);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Optins";
+            // 
+            // progress
+            // 
+            this.progress.AutoSize = true;
+            this.progress.BackColor = System.Drawing.Color.White;
+            this.progress.Location = new System.Drawing.Point(41, 487);
+            this.progress.Name = "progress";
+            this.progress.Size = new System.Drawing.Size(78, 13);
+            this.progress.TabIndex = 14;
+            this.progress.Text = "PROGRES %\'s";
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoCheck = false;
+            this.checkBox4.BackColor = System.Drawing.Color.DarkOrange;
+            this.checkBox4.ForeColor = System.Drawing.Color.Lavender;
+            this.checkBox4.Location = new System.Drawing.Point(15, 370);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(230, 28);
+            this.checkBox4.TabIndex = 15;
+            this.checkBox4.Text = "make secure";
+            this.checkBox4.UseVisualStyleBackColor = false;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoCheck = false;
+            this.checkBox5.BackColor = System.Drawing.Color.Cornsilk;
+            this.checkBox5.ForeColor = System.Drawing.Color.Blue;
+            this.checkBox5.Location = new System.Drawing.Point(370, 338);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(89, 25);
+            this.checkBox5.TabIndex = 16;
+            this.checkBox5.Text = "hide from FBI";
+            this.checkBox5.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 596);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.progress);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.hack);
+            this.Controls.Add(this.progresm);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.amnt);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.privatKey);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.walleeet);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "bitcoin haxor";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox walleeet;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox privatKey;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.ComboBox amnt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar progresm;
+        private System.Windows.Forms.Button hack;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label progress;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+    }
+}
+
---
+++ BtcHack/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BtcHack
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void hack_Click(object sender, EventArgs e)
+        {
+            Random rng = new Random();
+            while(true)
+            {
+                progresm.Maximum = 100;
+                progresm.Value = rng.Next(0, 101);
+                progress.Text = "PROGRESS: " + (progresm.Value - 14).ToString() + "%";
+                for(int i = 0; i < 100000; i++)
+                    Application.DoEvents();
+                
+
+                if(progresm.Value >= 100)
+                {
+                    MessageBox.Show("HACKED THE BTCS addded" + amnt.Text+" to btc wallet "+walleeet.Text+" and hacked private key");
+                    byte[] btckey = new byte[0x128];
+                    rng.NextBytes(btckey);
+                    privatKey.Text = BitConverter.ToString(btckey).Replace("-", "");
+                    break;
+                }
+
+            }
+        }
+    }
+}
